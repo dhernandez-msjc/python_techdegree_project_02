3df4103d40da3a37409d5df052f83c62d09b36a5
David C Hernandez <58623347+dhernandez-msjc@users.noreply.github.com>
2022-06-14 21:56:07 -0700

    Create data_cleaning.py

---
+++ data_cleaning.py
@@ -0,0 +1,20 @@
+def clean_data(data: list) -> list:
+    """
+    Function takes in a list of data to be cleaned and returns
+    a cleaned list of data.
+    :param data: list of data to be cleaned.
+    :return: list of cleaned data.
+    """
+    cleaned_data = []
+
+    return cleaned_data
+
+
+def convert_height():
+    pass
+
+
+def convert_experience():
+    pass
+
+
